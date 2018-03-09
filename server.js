cec080dd6bbe7a8c19e6dbedfc67d5a485965e05
Varun KH <varun.kh@globaldelight.com>
2018-03-09 16:14:47 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+})
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    let log = `${now} : ${req.method} and ${req.url}`;
+
+    fs.appendFile('server.log',log + '/n', (err) =>{
+        if(err){
+            console.log('Unable to append file');
+        }
+    })
+    console.log(log);
+    next();
+})
+
+app.use((req, res, next) =>{
+    res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req,res) =>{
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        h1Tag : 'Welcome to Home page'
+    });
+});
+
+app.get('/about', (req,res) =>{
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+        h1Tag : 'Welcome to About page'
+    });
+});
+
+app.get('/bad', (req,res) =>{
+    res.send({
+        error : 'Bad Request'
+    });
+});
+
+app.listen(3000);
