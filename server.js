a4a5bde7a40605ed170e0ea3fe1b1d7e16c23187
Varun KH <varun.kh@globaldelight.com>
2018-03-09 19:43:47 +0530

    Added portfolio page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req,res) =>{
     });
 });
 
+app.get('/projects', (req,res) =>{
+    res.render('projects.hbs',{
+        pageTitle : 'Projects Page',
+        h1Tag : 'Porfolio'
+    });
+});
+
 app.get('/bad', (req,res) =>{
     res.send({
         error : 'Bad Request'
